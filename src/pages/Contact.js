01eb6bc994d82907a0e62c384fa1ff924c22735d
Faizaa Hayatt <hayatfaiza2@gmail.com>
2024-01-04 14:09:12 +0500

    ,
    .

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,38 +1,45 @@
-import React from 'react'
-import '../style/contact.css'
+// Contact.js
+import React from 'react';
+import '../style/contact.css';
 
 function Contact() {
+
+
   return (
     <div className="contact-container">
-      <div className="contact-details">
+      <div className='container'>
         <h1>Contact Us</h1>
-        <p>Have any questions or concerns? Reach out to us below.</p>
+        <p>
+          If you have any questions or inquiries, feel free to contact us using the form below or through the provided contact information.
+        </p>
 
-        <div className="contact-info">
-          <p>Send us a message:</p>
-          <p>Email: info@yourjewelry.com</p>
-          <p>Follow us on social media:</p>
-          <p>Facebook: /yourjewelry</p>
-          <p>Instagram: @yourjewelry</p>
-        </div>
-      </div>
+        <form className="contact-form">
 
-      <div className="contact-form">
-        <form action="#" method="post">
-          <label htmlFor="name">Your Name</label>
-          <input type="text" id="name" name="name" required />
+          <input type="text" id="name" name="name" placeholder='Name' />
+          <div className='mid'>
+            <input type="subject" id="subject" name="subject" placeholder='Subject' required />
+            <input type="email" id="email" name="email" placeholder='Email' required />
+          </div>
 
-          <label htmlFor="email">Your Email</label>
-          <input type="email" id="email" name="email" required />
-
-          <label htmlFor="message">Your Message</label>
-          <textarea id="message" name="message" rows="5" required></textarea>
+          <textarea id="message" name="message" rows="4" placeholder='Message' required></textarea>
 
           <button type="submit">Submit</button>
         </form>
       </div>
+<div className='container'>
+      <h2>Contact information</h2>
+      <p >Connect with us for any inquiries or collaborations! We welcome your messages and feedback. Reach out to us through email or social media, and we'll be happy to respond. Let's explore opportunities together!</p>
+      </div>
+      <div className="contact-info">
+
+        <div>
+
+          <p> info@yourjewelrywebsite.com</p></div>
+        <div>  <p> +1 (555) 123-4567</p></div>
+        <div> <p>123 Main Street, Cityville, State, Country</p></div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
